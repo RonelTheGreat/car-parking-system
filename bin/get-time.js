02c95eb109c55e9d9dd0ef1c173c569a1a184412
ronel carlo berino <ronelcarlo.berino@gmail.com>
2019-07-13 15:54:59 +0800

    added helpful comments for future reference

--- bin/get-time.js
+++ bin/get-time.js
@@ -1,12 +1,17 @@
 // calculate remaining time
+// params {expiration time}
 function getTime(endTime) {
+    // grab the time now
     let now = new Date().getTime();
+    // calculate the remaining time
     let t = endTime - now;
 
+    // calculate and get hour, min, sec
     let hour = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let min = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     let sec = Math.floor((t % (1000 * 60)) / 1000);
     
+    // store hour, min, sec
     let obj = {
         hour: hour,
         min: min,
