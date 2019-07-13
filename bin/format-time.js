21bd31eac907e10273230e612520fb90f774eac1
ronel carlo berino <ronelcarlo.berino@gmail.com>
2019-07-13 15:54:45 +0800

    added helpful comments for future reference

--- bin/format-time.js
+++ bin/format-time.js
@@ -1,17 +1,24 @@
-// format time to h:m AM/PM
-// PARAMS {date, durationHours[number], durationMinutes[number]}
-// returns {start time, end time, end time in milliseconds}
+// format time to hh:mm AM/PM (e.g. 12:00 AM)
+// params {date with time to format}
+// returns the formatted time in hh:mm AM or PM
 function formatTime(date) {
 
+    // get hour and minute
     let hours = date.getHours();
     let minutes = date.getMinutes();
+    
+    // get time of the day, PM or AM
+    // if hour is greater than 12, then it is PM else it is AM
     let ampm = hours >= 12 ? 'PM' : 'AM';
 
+    // get hour
     hours = hours % 12;
     hours = hours ? hours : 12;
+
+    // if minute less than 10, add 0 to the front (e.g 09, 08 etc.)
     minutes = `${(minutes < 10) ? '0' + minutes : minutes}`;
 
-
+    // store the formatted time
     let obj = {
         time: `${hours}:${minutes} ${ampm}`,
     };
