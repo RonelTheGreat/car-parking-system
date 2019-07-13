6764ee11f23be8519ad679780a189b862224faf1
ronel carlo berino <ronelcarlo.berino@gmail.com>
2019-07-13 15:53:53 +0800

    added helpful comments for future reference

--- bin/create-admin.js
+++ bin/create-admin.js
@@ -1,17 +1,23 @@
 const bcrypt    = require('bcrypt'),
       User      = require('../models/User');
 
-
+// create an admin with hashed password
+// params {username, password}
 let createAdmin = (username, password) => {
 
+    // hash the given password
+    // output would be (e.g syx89sfx.123sf#$df-s9)
     bcrypt.hash(password, 10, (err, hashedPW) => {
     
+        // store hashed password and username
+        // set admin to true
         let newUser = {
             username: username,
             password: hashedPW,
             isAdmin: true,
         };
     
+        // create new admin and save to DB
         User.create(newUser, (err, newUser) => {
     
             if (err) return console.log('Error in creating a new admin ...');
